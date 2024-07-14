8c63bb5d5404e4c7505ac1b68a3142f94003952f
AdhvikaS <adhvikasundaramurali@gmail.com>
2024-07-14 20:12:11 +0530

    Add files via upload

---
+++ main (3).py
@@ -0,0 +1,13 @@
+#Adhvika
+
+def gem(number):
+    even = []
+    odd = []
+    for n in number:
+        if n%2==0:
+            even.append(n**2)
+        elif n%2!=0:
+            odd.append(n)
+    return even+odd
+number=[34,7,5,72,82]
+print(gem(number))
